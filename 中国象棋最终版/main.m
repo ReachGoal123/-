acc65a10b6a859bb26afde2a954a2afa89d28f45
D.S.T LIANG <dstliang@DSTtekiMacBook-Pro.local>
2013-11-27 14:35:01 +0800

    Initial Commit

---
+++ 中国象棋最终版/main.m
@@ -0,0 +1,22 @@
+//
+//  main.m
+//  中国象棋最终版
+//
+//  Created by D.S.T LIANG on 13-11-27.
+//  Copyright (c) 2013年 李彦亮. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[])
+{
+
+    @autoreleasepool {
+        
+        // insert code here...
+        NSLog(@"Hello, World!");
+        
+    }
+    return 0;
+}
+
